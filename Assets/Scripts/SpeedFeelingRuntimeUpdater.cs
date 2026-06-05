d91daf03046ae21a287791d80f9847fec33374d5
ski-verse <persson.mce@outlook.com>
2026-06-05 13:31:00 +0200

    Strengthen perceived speed cues

--- Assets/Scripts/SpeedFeelingRuntimeUpdater.cs
+++ Assets/Scripts/SpeedFeelingRuntimeUpdater.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 public class SpeedFeelingRuntimeUpdater : MonoBehaviour
 {
     public const float CameraHeightMultiplier = 1.15f;
-    public const float EnhancedMaxFieldOfView = 100f;
-    public const float EnhancedSpeedForMaxFieldOfViewKmh = 68f;
+    public const float EnhancedMaxFieldOfView = 104f;
+    public const float EnhancedSpeedForMaxFieldOfViewKmh = 60f;
     public const float MotionCueCourseLengthMeters = 5000f;
-    public const float MotionCueStartDistanceMeters = 18f;
-    public const float MotionCueSpacingMeters = 12.5f;
-    public const float MotionCueLateralOffset = 5.65f;
-    public const float MotionCueHalfWidth = 0.08f;
+    public const float MotionCueStartDistanceMeters = 14f;
+    public const float MotionCueSpacingMeters = 8f;
+    public const float MotionCueLateralOffset = 5.45f;
+    public const float MotionCueHalfWidth = 0.07f;
+    public const float EdgeFlowCueSpacingMeters = 9.5f;
+    public const float EdgeFlowCueLateralOffset = 4.55f;
+    public const float EdgeFlowCueHalfWidth = 0.055f;
 
     private bool cameraConfigured;
     private bool motionCuesCreated;
@@ -74,6 +77,12 @@ public class SpeedFeelingRuntimeUpdater : MonoBehaviour
         return Mathf.CeilToInt((courseLengthMeters - startDistanceMeters) / spacingMeters) * 2;
     }
 
+    public static int CalculateTotalMotionCueCount(float courseLengthMeters)
+    {
+        return CalculateMotionCueCount(courseLengthMeters, MotionCueStartDistanceMeters, MotionCueSpacingMeters) +
+               CalculateMotionCueCount(courseLengthMeters, MotionCueStartDistanceMeters + EdgeFlowCueSpacingMeters * 0.5f, EdgeFlowCueSpacingMeters);
+    }
+
     private void ConfigureCameraIfAvailable()
     {
         var followCamera = Object.FindFirstObjectByType<FollowCamera>();
@@ -104,6 +113,13 @@ public class SpeedFeelingRuntimeUpdater : MonoBehaviour
             cueCount += 2;
         }
 
+        for (var z = MotionCueStartDistanceMeters + EdgeFlowCueSpacingMeters * 0.5f; z < MotionCueCourseLengthMeters; z += EdgeFlowCueSpacingMeters)
+        {
+            CreateEdgeFlowCue(root.transform, z, -EdgeFlowCueLateralOffset, cueCount);
+            CreateEdgeFlowCue(root.transform, z + EdgeFlowCueSpacingMeters * 0.5f, EdgeFlowCueLateralOffset, cueCount + 1);
+            cueCount += 2;
+        }
+
         motionCuesCreated = true;
     }
 
@@ -114,12 +130,28 @@ public class SpeedFeelingRuntimeUpdater : MonoBehaviour
         cue.transform.SetParent(parent, false);
 
         var position = CoursePath.PointAtDistance(zPosition, lateralOffset);
-        position.y += 0.24f;
+        position.y += 0.25f;
         cue.transform.position = position;
         cue.transform.rotation = CoursePath.RotationAtDistance(zPosition);
-        cue.transform.localScale = new Vector3(MotionCueHalfWidth * 2f, 0.48f, 0.9f);
+        cue.transform.localScale = new Vector3(MotionCueHalfWidth * 2f, 0.5f, 0.95f);
 
         var color = index % 4 < 2 ? Color.white : new Color(0.12f, 0.45f, 0.95f);
+        cue.GetComponent<Renderer>().material.color = color;
+    }
+
+    private static void CreateEdgeFlowCue(Transform parent, float zPosition, float lateralOffset, int index)
+    {
+        var cue = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cue.name = "Road Edge Flow Cue";
+        cue.transform.SetParent(parent, false);
+
+        var position = CoursePath.PointAtDistance(zPosition, lateralOffset);
+        position.y += 0.035f;
+        cue.transform.position = position;
+        cue.transform.rotation = CoursePath.RotationAtDistance(zPosition);
+        cue.transform.localScale = new Vector3(EdgeFlowCueHalfWidth * 2f, 0.035f, 1.45f);
+
+        var color = index % 4 < 2 ? new Color(0.92f, 0.96f, 1f) : new Color(0.16f, 0.58f, 0.95f);
         cue.GetComponent<Renderer>().material.color = color;
     }
 }
