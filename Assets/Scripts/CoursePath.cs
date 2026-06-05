3a0947c2ad17ced931fff46bcda6d0ffee547e87
ski-verse <persson.mce@outlook.com>
2026-06-05 09:36:10 +0200

    Add dramatic sweeping course profile

--- Assets/Scripts/CoursePath.cs
+++ Assets/Scripts/CoursePath.cs
@@ -2,34 +2,50 @@ using UnityEngine;
 
 public static class CoursePath
 {
-    private const float PrimaryCurveAmplitude = 13f;
-    private const float SecondaryCurveAmplitude = 4.5f;
-    private const float PrimaryCurveFrequency = 0.0038f;
-    private const float SecondaryCurveFrequency = 0.00145f;
+    private const float PrimaryCurveAmplitude = 42f;
+    private const float SecondaryCurveAmplitude = 24f;
+    private const float AccentCurveAmplitude = 8f;
+    private const float PrimaryCurveFrequency = 0.0048f;
+    private const float SecondaryCurveFrequency = 0.00175f;
+    private const float AccentCurveFrequency = 0.0105f;
+    private const float SecondaryCurvePhase = 160f;
+    private const float PrimaryHillAmplitude = 9f;
+    private const float SecondaryHillAmplitude = 13f;
+    private const float PrimaryHillFrequency = 0.0062f;
+    private const float SecondaryHillFrequency = 0.0024f;
+    private const float PrimaryHillPhase = -80f;
+    private const float SecondaryHillPhase = 220f;
     private const float DirectionSampleDistance = 8f;
 
     public static float CenterXAtDistance(float zPosition)
     {
         return Mathf.Sin(zPosition * PrimaryCurveFrequency) * PrimaryCurveAmplitude
-            + Mathf.Sin(zPosition * SecondaryCurveFrequency) * SecondaryCurveAmplitude;
+            + Mathf.Sin((zPosition + SecondaryCurvePhase) * SecondaryCurveFrequency) * SecondaryCurveAmplitude
+            + Mathf.Sin(zPosition * AccentCurveFrequency) * AccentCurveAmplitude;
+    }
+
+    public static float HeightAtDistance(float zPosition)
+    {
+        return Mathf.Sin((zPosition + PrimaryHillPhase) * PrimaryHillFrequency) * PrimaryHillAmplitude
+            + Mathf.Sin((zPosition + SecondaryHillPhase) * SecondaryHillFrequency) * SecondaryHillAmplitude;
     }
 
     public static Vector3 CenterPointAtDistance(float zPosition)
     {
-        return new Vector3(CenterXAtDistance(zPosition), 0f, zPosition);
+        return new Vector3(CenterXAtDistance(zPosition), HeightAtDistance(zPosition), zPosition);
     }
 
     public static Vector3 DirectionAtDistance(float zPosition)
     {
-        var previousX = CenterXAtDistance(zPosition - DirectionSampleDistance);
-        var nextX = CenterXAtDistance(zPosition + DirectionSampleDistance);
-        return new Vector3(nextX - previousX, 0f, DirectionSampleDistance * 2f).normalized;
+        var previousPoint = CenterPointAtDistance(zPosition - DirectionSampleDistance);
+        var nextPoint = CenterPointAtDistance(zPosition + DirectionSampleDistance);
+        return (nextPoint - previousPoint).normalized;
     }
 
     public static Vector3 RightAtDistance(float zPosition)
     {
         var direction = DirectionAtDistance(zPosition);
-        return new Vector3(direction.z, 0f, -direction.x).normalized;
+        return Vector3.Cross(Vector3.up, direction).normalized;
     }
 
     public static Vector3 PointAtDistance(float zPosition, float lateralOffset)
