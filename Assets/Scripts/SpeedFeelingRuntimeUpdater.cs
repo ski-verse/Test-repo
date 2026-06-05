d3af89784878eb0c1eea20b3a6531e79c113ace1
ski-verse <persson.mce@outlook.com>
2026-06-05 11:48:06 +0200

    Align motion cue count with placement loop

--- Assets/Scripts/SpeedFeelingRuntimeUpdater.cs
+++ Assets/Scripts/SpeedFeelingRuntimeUpdater.cs
@@ -71,7 +71,7 @@ public class SpeedFeelingRuntimeUpdater : MonoBehaviour
             return 0;
         }
 
-        return Mathf.FloorToInt((courseLengthMeters - startDistanceMeters) / spacingMeters) * 2;
+        return (Mathf.FloorToInt((courseLengthMeters - startDistanceMeters) / spacingMeters) + 1) * 2;
     }
 
     private void ConfigureCameraIfAvailable()
