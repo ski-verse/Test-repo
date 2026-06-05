1f7e2c6c49f8767392d0653661a8100e3a00840b
ski-verse <persson.mce@outlook.com>
2026-06-05 19:14:28 +0200

    Add adventure character roller skier tests

---
+++ Assets/Tests/EditMode/AdventureCharacterRollerSkierRuntimeUpdaterTests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+public sealed class AdventureCharacterRollerSkierRuntimeUpdaterTests
+{
+    [Test]
+    public void AdventureCharacterConfiguration_UsesImportedAdventureCharacterPrefab()
+    {
+        Assert.AreEqual("Assets/Adventure_Character/Prefabs/Man_01.prefab", AdventureCharacterRollerSkierRuntimeUpdater.AdventureCharacterPrefabPath);
+        Assert.AreEqual("Adventure Character Roller Skier Applied", AdventureCharacterRollerSkierRuntimeUpdater.AdventureCharacterAppliedMarkerName);
+        Assert.AreEqual("Adventure Character Roller Skier", AdventureCharacterRollerSkierRuntimeUpdater.HumanoidRootName);
+    }
+}
