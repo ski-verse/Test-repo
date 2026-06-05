038c28501f6d7f65df9ccc501d0c73467bb802eb
ski-verse <persson.mce@outlook.com>
2026-06-05 16:56:15 +0200

    Test refined roller skier realism details

--- Assets/Tests/EditMode/RollerSkierVisualModelTests.cs
+++ Assets/Tests/EditMode/RollerSkierVisualModelTests.cs
@@ -67,6 +67,16 @@ public class RollerSkierVisualModelTests
     }
 
     [Test]
+    public void HumanSilhouettePass_RefinesGripBackAndHipRealism()
+    {
+        Assert.GreaterOrEqual(SkierHumanSilhouetteRuntimeUpdater.PolePressureLineRadius, 0.04f);
+        Assert.GreaterOrEqual(SkierHumanSilhouetteRuntimeUpdater.VisibleHandClampRadius, 0.092f);
+        Assert.GreaterOrEqual(SkierHumanSilhouetteRuntimeUpdater.UpperBackRidgeWidth, 0.62f);
+        Assert.GreaterOrEqual(SkierHumanSilhouetteRuntimeUpdater.RearShoulderPlaneWidth, 0.26f);
+        Assert.GreaterOrEqual(SkierHumanSilhouetteRuntimeUpdater.NaturalHipRoundness, 0.22f);
+    }
+
+    [Test]
     public void HumanSilhouettePass_AttachesReadableHumanDetailsToAnimatedRig()
     {
         var skier = new GameObject("Low Poly Roller Skier");
@@ -83,19 +93,64 @@ public class RollerSkierVisualModelTests
         Assert.IsNotNull(FindChildRecursive(animator.torso, "Human Central Spine Seam"));
         Assert.IsNotNull(FindChildRecursive(animator.torso, "Human Left Scapula Shadow"));
         Assert.IsNotNull(FindChildRecursive(animator.torso, "Human Lat Shadow Left"));
+        Assert.IsNotNull(FindChildRecursive(animator.torso, "Gameplay Upper Back Ridge"));
+        Assert.IsNotNull(FindChildRecursive(animator.torso, "Left Rear Shoulder Plane"));
         Assert.IsNotNull(FindChildRecursive(animator.hips, "Human Black Shorts Block"));
         Assert.IsNotNull(FindChildRecursive(animator.hips, "Human Left Glute Accent"));
         Assert.IsNotNull(FindChildRecursive(animator.hips, "Human Shorts Leg Split"));
+        Assert.IsNotNull(FindChildRecursive(animator.hips, "Left Natural Hip Roundness"));
         Assert.IsNotNull(FindChildRecursive(animator.leftHand, "Visible Glove Grip Wrap"));
         Assert.IsNotNull(FindChildRecursive(animator.rightHand, "Visible Glove Grip Wrap"));
+        Assert.IsNotNull(FindChildRecursive(animator.leftHand, "Visible Hand Clamp Around Pole"));
+        Assert.IsNotNull(FindChildRecursive(animator.rightHand, "Visible Hand Clamp Around Pole"));
         Assert.IsNotNull(FindChildRecursive(animator.leftPole, "Gameplay Rear Visible Pole Shaft"));
         Assert.IsNotNull(FindChildRecursive(animator.rightPole, "Gameplay Rear Visible Pole Shaft"));
         Assert.IsNotNull(FindChildRecursive(animator.leftPole, "Gameplay Rear Pole Plant Dot"));
         Assert.IsNotNull(FindChildRecursive(animator.rightPole, "Gameplay Rear Pole Plant Dot"));
+        Assert.IsNotNull(FindChildRecursive(animator.leftPole, "Visible Pole Pressure Line"));
+        Assert.IsNotNull(FindChildRecursive(animator.rightPole, "Visible Pole Pressure Line"));
         Assert.AreEqual(SkierHumanSilhouetteRuntimeUpdater.NaturalUpperArmRadius, FindChildRecursive(animator.leftArm, "Relaxed Upper Arm").localScale.x, 0.001f);
         Assert.AreEqual(SkierHumanSilhouetteRuntimeUpdater.NaturalUpperArmRadius, FindChildRecursive(animator.rightArm, "Relaxed Upper Arm").localScale.x, 0.001f);
         Assert.AreEqual(animator.leftHand, animator.leftPole.parent);
         Assert.AreEqual(animator.rightHand, animator.rightPole.parent);
+
+        Object.DestroyImmediate(skier);
+    }
+
+    [Test]
+    public void DoublePolePose_CompressesBodyWhileFeetStayConnectedToSkis()
+    {
+        var skier = new GameObject("Animated Skier");
+        var animator = skier.AddComponent<RollerSkierAnimator>();
+        animator.torso = new GameObject("Torso").transform;
+        animator.hips = new GameObject("Hips").transform;
+        animator.leftFoot = new GameObject("Left Foot").transform;
+        animator.rightFoot = new GameObject("Right Foot").transform;
+        animator.leftSki = new GameObject("Left Ski").transform;
+        animator.rightSki = new GameObject("Right Ski").transform;
+        animator.torso.SetParent(skier.transform, false);
+        animator.hips.SetParent(skier.transform, false);
+        animator.leftFoot.SetParent(skier.transform, false);
+        animator.rightFoot.SetParent(skier.transform, false);
+        animator.leftSki.SetParent(skier.transform, false);
+        animator.rightSki.SetParent(skier.transform, false);
+        animator.torso.localPosition = new Vector3(0f, 1.1f, 0f);
+        animator.hips.localPosition = new Vector3(0f, 0.95f, 0f);
+        animator.leftFoot.localPosition = new Vector3(-0.2f, 0.15f, 0.08f);
+        animator.rightFoot.localPosition = new Vector3(0.2f, 0.15f, 0.08f);
+        animator.leftSki.localPosition = new Vector3(-0.2f, 0f, 0.08f);
+        animator.rightSki.localPosition = new Vector3(0.2f, 0f, 0.08f);
+        var leftFootToSki = animator.leftFoot.localPosition - animator.leftSki.localPosition;
+        var rightFootToSki = animator.rightFoot.localPosition - animator.rightSki.localPosition;
+
+        animator.ApplyPose(0.46f);
+
+        Assert.Less(animator.torso.localPosition.y, 1.1f);
+        Assert.Less(animator.hips.localPosition.y, 0.95f);
+        Assert.AreEqual(leftFootToSki.y, (animator.leftFoot.localPosition - animator.leftSki.localPosition).y, 0.002f);
+        Assert.AreEqual(leftFootToSki.z, (animator.leftFoot.localPosition - animator.leftSki.localPosition).z, 0.002f);
+        Assert.AreEqual(rightFootToSki.y, (animator.rightFoot.localPosition - animator.rightSki.localPosition).y, 0.002f);
+        Assert.AreEqual(rightFootToSki.z, (animator.rightFoot.localPosition - animator.rightSki.localPosition).z, 0.002f);
 
         Object.DestroyImmediate(skier);
     }
